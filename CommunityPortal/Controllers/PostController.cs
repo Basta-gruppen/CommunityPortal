622fa6a3bfd4ffe2b5797bbd1b9a8bf1834af125
Tim Weinitz <weinitz.tim@gmail.com>
2022-01-10 17:27:12 +0100

    Add PostController and View

---
+++ CommunityPortal/Controllers/PostController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommunityPortal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunityPortal.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly List<Post> _posts = new List<Post>
+        {
+            new Post
+            {
+                User = new ApplicationUser
+                {
+                    UserName = "Tim"
+                },
+
+                Id = Guid.NewGuid().ToString(),
+                Subject = "This is a post",
+                PostTags = new List<PostTag>
+                {
+                    new PostTag
+                    {
+                        TagId = Guid.NewGuid().ToString(),
+                        Tag = new Tag
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "Foo"
+                        }
+                    }
+                }
+            },
+            new Post
+            {
+                Id = Guid.NewGuid().ToString(),
+                User = new ApplicationUser
+                {
+                    UserName = "Martinyas"
+                },
+                Subject = "Off-Topic",
+
+                PostTags = new List<PostTag>
+                {
+                    new PostTag
+                    {
+                        TagId = Guid.NewGuid().ToString(),
+                        Tag = new Tag
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "Foo"
+                        }
+                    }
+                }
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(_posts);
+        }
+        
+        [HttpGet]
+        [Route("/People/{id:int}")]
+        public new IActionResult View(int id)
+        {
+            return View(_posts.First());
+        }
+    }
+}
