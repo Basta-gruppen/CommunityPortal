2cd24832905750f5c8b7b8391daf39b75921ac81
arqtik <martynascamets1@gmail.com>
2022-01-10 12:45:54 +0100

    Create SubForumController.cs
    Added subforum index with dummy data

---
+++ CommunityPortal/Controllers/SubForumController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using CommunityPortal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunityPortal.Controllers
+{
+    public class SubForumController : Controller
+    {
+        // GET
+        public IActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return NotFound();
+            }
+            
+            SubForum subForum = new SubForum()
+            {
+                Name = "Dummy SubForum",
+                Threads = new List<Thread>()
+                {
+                    new Thread()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Subject = "My first thread!",
+                        User = new ApplicationUser()
+                        {
+                            UserName = "Martynas"
+                        },
+                        TimeStamp = DateTime.Now
+                    },
+                    new Thread()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Subject = "My second thread!",
+                        User = new ApplicationUser()
+                        {
+                            UserName = "Tim"
+                        },
+                        TimeStamp = DateTime.Now
+                    },
+                    new Thread()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Subject = "My third thread!",
+                        User = new ApplicationUser()
+                        {
+                            UserName = "Monika"
+                        },
+                        TimeStamp = DateTime.Now
+                    },
+                    new Thread()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Subject = "My fourth thread!",
+                        User = new ApplicationUser()
+                        {
+                            UserName = "Ekram"
+                        },
+                        TimeStamp = DateTime.Now
+                    }
+                }
+            };
+            
+            return View(subForum);
+        }
+    }
+}
