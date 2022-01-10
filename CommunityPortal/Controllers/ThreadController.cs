2e73451f518268cd6148c6139e456669e05ffd46
arqtik <martynascamets1@gmail.com>
2022-01-10 12:47:52 +0100

    Create ThreadController.cs

---
+++ CommunityPortal/Controllers/ThreadController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommunityPortal.Controllers
+{
+    public class ThreadController : Controller
+    {
+        // GET
+        public IActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return NotFound();
+            }
+            
+            return View();
+        }
+    }
+}
