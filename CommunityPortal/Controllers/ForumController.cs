1a1c3cf5cce39cdb5dc3ea781740738d5e8dff0e
arqtik <martynascamets1@gmail.com>
2022-01-10 12:19:41 +0100

    Create ForumController.cs
    With dummy forum data

---
+++ CommunityPortal/Controllers/ForumController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using CommunityPortal.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunityPortal.Controllers
+{
+    public class ForumController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Forum> forums = new List<Forum>()
+            {
+                new Forum()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Introductions",
+                    SubForums = new List<SubForum>()
+                    {
+                        new SubForum()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "New People",
+                            Description = "New people can post their introductions here."
+                        },
+                        new SubForum()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "Veterans",
+                            Description = "Veterans can introduce themselves here."
+                        }
+                    }
+                },
+                new Forum()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Off-Topic",
+                    SubForums = new List<SubForum>()
+                    {
+                        new SubForum()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "Gaming",
+                            Description = "Threads about gaming."
+                        },
+                        new SubForum()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "Health & Safety",
+                            Description = "Discussions regard people's health & safety."
+                        }
+                    }
+                }
+            };
+
+            return View(forums);
+        }
+        
+        
+    }
+}
