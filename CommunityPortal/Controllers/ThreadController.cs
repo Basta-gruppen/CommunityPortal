0cd96b9f7a3c4a1380112423f5f587b1449653ca
arqtik <martynascamets1@gmail.com>
2022-01-10 13:34:29 +0100

    Update ThreadController.cs
    Added dummy data to thread controller

--- CommunityPortal/Controllers/ThreadController.cs
+++ CommunityPortal/Controllers/ThreadController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using CommunityPortal.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CommunityPortal.Controllers
 {
@@ -11,8 +14,51 @@ namespace CommunityPortal.Controllers
             {
                 return NotFound();
             }
+
+            ApplicationUser poster = new ApplicationUser()
+            {
+                UserName = "DummyUser",
+                ImageURL = "https://i.pravatar.cc/100?img=1"
+            };
             
-            return View();
+            Thread thread = new Thread()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Subject = "Dummy Subject for this thread",
+                User = poster,
+                TimeStamp = DateTime.Now,
+                Replies = new List<Reply>()
+                {
+                    new Reply()
+                    {
+                        User = poster,
+                        TimeStamp = DateTime.Now,
+                        Content = "Hello, so this should be the first reply, a.k.a the content of the thread"
+                    },
+                    new Reply()
+                    {
+                        User = new ApplicationUser()
+                        {
+                            UserName = "ReplyingUser1",
+                            ImageURL = "https://i.pravatar.cc/100?img=2"
+                        },
+                        TimeStamp = DateTime.Now,
+                        Content = "This should be the first reply to the thread, let's hope it works!"
+                    },
+                    new Reply()
+                    {
+                        User = new ApplicationUser()
+                        {
+                            UserName = "ReplyingUser2",
+                            ImageURL = "https://i.pravatar.cc/100?img=3"
+                        },
+                        TimeStamp = DateTime.Now,
+                        Content = "This should be the second reply to the thread!"
+                    }
+                }
+            };
+            
+            return View(thread);
         }
     }
 }
