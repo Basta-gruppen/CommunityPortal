bfb1393055bf1398eca3e9ca4d879448616f3a6a
MonikaVyas1989 <89772349+MonikaVyas1989@users.noreply.github.com>
2022-01-13 22:27:59 +0100

    SelectUser for private message

---
+++ CommunityPortal/wwwroot/js/Message.js
@@ -0,0 +1,9 @@
+﻿ 
+
+function SendMessage() {
+    messageId = document.getElementById("message").value
+    $.post("/Conversation/CreateConversation", { id: messageId }, function (data) {
+        console.log(data)
+        $("#getMessage").html(data);
+    });
+}
