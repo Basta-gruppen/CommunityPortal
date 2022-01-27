d59e2ac1d680fe264600086f0326dcd5748da9d6
MonikaVyas1989 <89772349+MonikaVyas1989@users.noreply.github.com>
2022-01-27 14:44:12 +0100

    ConversationQuery

---
+++ CommunityPortal/Controllers/MessageController.cs
@@ -0,0 +1,88 @@
+﻿using CommunityPortal.Data;
+using CommunityPortal.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunityPortal.Controllers
+{
+    public class MessageController : Controller
+    { 
+        private readonly ApplicationDbContext dbContext;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public MessageController(ApplicationDbContext context, UserManager<ApplicationUser> applicationUser)
+        {
+            dbContext = context;
+            userManager = applicationUser;
+        }
+        public IActionResult Index(string id)
+        {
+            List<Message> message = dbContext.Messages.Include(m => m.User).ThenInclude(c => c.UserConversations).Where
+                (c => c.ConversationId == id).OrderByDescending(m=>m.TimeStamp).ToList();
+
+            Conversation conversation = new Conversation()
+            {
+                Messages = message
+            };
+
+
+            //if (message.Count<=0)
+            //{
+            //    return View("Create");
+            //}
+
+            return View(conversation);
+
+            
+        }
+        public IActionResult Create()
+        { return View(); }
+        [HttpPost]
+        public IActionResult Create(string content,string id)
+        { 
+            Message messages = new Message()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Content = content,
+                ConversationId = id,
+                UserId = userManager.GetUserId(User),
+                TimeStamp = DateTime.Now 
+            };
+            dbContext.Messages.Add(messages);
+            dbContext.SaveChanges();
+            return View();
+            //return RedirectToAction("MessageInConversation");
+        }
+
+
+        //public IActionResult MessageInConversation() { return View(); }
+        //[HttpPost]
+        public IActionResult MessageInConversation(string id) 
+        {
+            //List<Message> messages = dbContext.Messages
+            //    .Include(m => m.Conversation)
+            //    .ThenInclude(m => m.Messages)
+            //    .Where(m=>m.ConversationId==id).ToList();
+            Conversation conversation = dbContext.Conversations
+             .Include(m => m.Messages)
+             .ThenInclude(u => u.User)
+             .FirstOrDefault(c => c.Id == id);
+            return PartialView("_MessagePartialView", conversation);
+        }
+
+        public IActionResult Delete(string id)
+        {
+
+            dbContext.Messages.Remove(dbContext.Messages.Find(id));
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+           
+        }
+
+    }
+}
---
+++ CommunityPortal/ViewModels/CreateConversationViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunityPortal.ViewModels
+{
+    public class CreateConversationViewModel
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Subject { get; set; }
+
+        public List<string> Users { get; set; }
+        public List<string> Messages { get; set; }
+    }
+}
