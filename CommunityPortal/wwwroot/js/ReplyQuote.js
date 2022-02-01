86756e227a0e841ac0a96f7fc942c7255028def5
arqtik <martynascamets1@gmail.com>
2022-02-01 15:15:57 +0100

    Update ReplyQuote.js
    Removed console.log

--- CommunityPortal/wwwroot/js/ReplyQuote.js
+++ CommunityPortal/wwwroot/js/ReplyQuote.js
@@ -1,6 +1,5 @@
 ﻿function SetReplyQuote(quoteId){
     $('#QuoteId').val(quoteId);
     let content = $(`#${quoteId}-content`).html();
-    console.log(content)
     $('#QuoteContent').val(content);
 }
